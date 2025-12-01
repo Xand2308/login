2b01a4b92a6fde1bbdc790163e5f651610322105
Xand2308 <xan372016@gmail.com>
2025-12-01 09:47:48 -0300

    Corrige index.html, adiciona programacao.html e arquivos de apoio

--- eventos.js
+++ eventos.js
@@ -1,14 +1,2 @@
-//Seleciona o botão ID
-
-var button = document.getElementById("button");
-var email = document.getElementById("email");
-var senha = document.getElementById("senha");
-
-button.addEventListener("click", function(){
-    if (email.value == "" || senha.value == ""){
-        alert("campo e-email ou senha não preenchidos");
-
-    }else{
-        alert("Campos preeenchidos com sucesso");
-    }
-});
+// Arquivo de apoio. Atualize conforme necessário.
+console.log('eventos.js carregado');
